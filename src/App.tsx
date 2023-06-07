cd3cba846e23f31dddd2d8661b2c352fea819fdb
boevDev <hg.hellstaff@gmail.com>
2023-06-07 22:03:19 +0500

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ const App: FC = () => {
     <>
       <Header />
       <Navbar />
+
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='about' element={<AboutPage />} />
